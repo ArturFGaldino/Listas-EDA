e036e9f5f0b24848c7a2226222b1d098b59cf678
Artur Fernandes Galdino <fernandesartur4@gmail.com>
2025-09-14 22:14:05 -0300

    email

---
+++ Lista-1/patinhos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int P, M, N;
+    char caminho[100000000];
+    scanf("%s", &P);
+    while(scanf("%d", caminho) != EOF) {
+        
+        }
+    return 0;
+}
